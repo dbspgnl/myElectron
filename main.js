f558321163b48787b8741a29e52c313ffde68727
khokhoa <dbspgnl@naver.com>
2022-09-15 15:06:01 +0900

    myElectron project init

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, Menu } = require("electron");
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 900,
+        height: 600
+    });
+    window.loadFile("index.html");
+    
+    // 커스텀 메뉴 템플릿
+    const template = [
+        {
+            label: "Nomentum",
+            submenu: [{role: "about"}, {type: "separator"}, {role: "quit"}],
+        },
+        {
+            label: "🎁",
+            submenu: [
+                {
+                    label: "New Background",
+                    click: () => {
+                        window.webContents.executeJavaScript(`
+                            getBackground();
+                        `);
+                    }
+                }, 
+                {type: "separator"}, 
+                {
+                    label: "Reset Preferences",
+                    click: () => {
+                        window.webContents.executeJavaScript(`
+                            localStorage.clear();
+                            window.location.reload();
+                        `);
+                    }
+                }
+            ],
+        }
+    ];
+
+    const customMenu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(customMenu);
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
