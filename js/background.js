43d2f6a17a25c3002301a5e117794ce47e44118b
khokhoa <dbspgnl@naver.com>
2022-09-25 21:39:45 +0900

    * backgroundImage > img 속성 방식으로 백그라운드 처리 (일렉트론 오류 해결)

--- js/background.js
+++ js/background.js
@@ -6,4 +6,11 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 // bgImage.src = `img/${chosenImage}`;
 // document.body.appendChild(bgImage);
 
-document.body.style.backgroundImage = `url('/img/${chosenImage}')`;
+// document.body.style.backgroundImage = `url('/img/${chosenImage}')`;
+// document.body.style.backgroundColor = "pink";
+// document.body.style.backgroundImage = `url('/img/0.jpg')`;
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+bgImage.id = "body-background"
+document.body.appendChild(bgImage);
