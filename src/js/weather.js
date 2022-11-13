8658431017fb73d94783260f3e5e077dba99402a
khokhoa <dbspgnl@naver.com>
2022-11-13 19:04:38 +0900

    + 날씨 처리 및 아이콘 적용 * 목록 삭제 오류 해결

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,20 +1,21 @@
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const API_KEY = "yourAppKey";
-    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    // fetch(url)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         console.log(data.name, data.weather[0].main);
-    //         const weather = document.querySelector("#weather span:first-child")
-    //         const city = document.querySelector("#weather span:last-child")
-    //         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    //         city.innerText = data.name; 
-    //     }).catch(() => console.log("not connect!"));
+    const API_KEY = "7d88b2d9bec3331892eb1be019f3ea6a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child")
+            const city = document.querySelector("#weather span:last-child")
+            const tmp =(data.main.temp-273.15).toFixed(1);
+            const imgURL = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            weather.innerHTML = `<img src=${imgURL} alt=${data.weather[0].main} /> ${tmp}ºC`;
+            city.innerText = " | "+data.name; 
+        }).catch(() => console.log("not connect!"));
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.")
 }
 
-// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
